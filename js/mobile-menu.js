669e4ecf4b4675f764446b9bec8f6c87f0347300
Vasily Pleshanov <vasilypleshanov@gmail.com>
2022-09-11 14:30:46 +0300

    added a mobile version of the menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,10 +1,16 @@
-burger();
+menu();
 
-function burger() {
+function menu() {
 	const burger = document.querySelector('.burger');
 	const menu = document.querySelector('.mobile-menu');
+	const menuBody = document.querySelector('.mobile-menu__body');
 
 	burger.addEventListener('click', () => {
+		burger.classList.toggle('_active');
+		menu.classList.toggle('_active');
+	});
+
+	menuBody.addEventListener('click', () => {
 		burger.classList.toggle('_active');
 		menu.classList.toggle('_active');
 	});
