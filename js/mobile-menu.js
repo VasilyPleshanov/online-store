49c6f8798f654f9114fe4f6cdc9eb5643ab1bb35
Vasily Pleshanov <vasilypleshanov@gmail.com>
2022-09-15 19:40:31 +0300

    added mobile submenu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,14 +4,42 @@ function menu() {
 	const burger = document.querySelector('.burger');
 	const menu = document.querySelector('.mobile-menu');
 	const menuBody = document.querySelector('.mobile-menu__body');
+  const linkMan = document.querySelector('#man');
+  const linkWoman = document.querySelector('#woman');
+  const submenu = document.querySelector('.header__mobile-submenu');
+  const submenuBody = document.querySelector('.mobile-menu__body_submenu');
 
 	burger.addEventListener('click', () => {
-		burger.classList.toggle('_active');
-		menu.classList.toggle('_active');
+		openCloseMenu();
 	});
 
 	menuBody.addEventListener('click', () => {
-		burger.classList.toggle('_active');
-		menu.classList.toggle('_active');
+		openCloseMenu();
 	});
+
+  linkMan.addEventListener('click', () => {
+		openCloseSubmenu();
+	});
+
+  linkWoman.addEventListener('click', () => {
+		openCloseSubmenu();
+	});
+
+  submenuBody.addEventListener('click', () => {
+		openCloseSubmenu();
+	});
+
+  function openCloseMenu() {
+    menu.classList.toggle('_active');
+    lockBody();
+  }
+
+  function openCloseSubmenu() {
+    submenu.classList.toggle('_active');
+    lockBody();
+  }
+
+  function lockBody() {
+    document.body.classList.toggle('_lock');
+  }
 }
